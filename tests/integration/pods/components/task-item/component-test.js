513300eaa6b2d6adfc29145aedb1f2eee393b35c
Evan Sosenko <razorx@evansosenko.com>
2015-08-29 15:46:57 -0700

    Add task-list and task-item components.

---
+++ tests/integration/pods/components/task-item/component-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('/task-item', 'Integration | Component | task item', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{task-item}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#task-item}}
+      template block text
+    {{/task-item}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
---
+++ tests/integration/pods/components/task-list/component-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('/task-list', 'Integration | Component | task list', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{task-list}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#task-list}}
+      template block text
+    {{/task-list}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
